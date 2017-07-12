7fdf8195df85e82a394e02c88e30c40e7df5ab94
vivikenken <vivikenken@gmail.com>
2017-07-13 00:08:03 +1000

    upload images function

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # uploadImageLab
 A file upload server allows user to upload images and response with the link to access the image.
+
+# dependencies
+    body-parser: "^1.17.2",
+    express: "^4.15.3",
+    multer: "^1.3.0",
+    mime: "^1.3.6",
+    unzip: "^0.1.11"
+
+# run command
+    node app.js
+
+# Localhost port
+    localhost:3000
---
+++ app.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var multer = require('multer');
+var bodyParser = require('body-parser');
+var fs = require('fs'),
+    path = require('path');
+var unzip = require('unzip');
+var app = express();
+
+app.use(bodyParser.json());
+app.use('/upload', express.static(path.join(__dirname, 'upload')));
+app.use('/upload/unzip', express.static(path.join(__dirname, 'upload/unzip')));
+
+
+var port = 3000;
+var fileName; 
+var Storage = multer.diskStorage({
+    destination: function (req, res, callback) {
+        callback(null, "./upload");
+    },
+    filename: function (req, res, callback) {
+        var dateTimeStamp = Date.now();
+        fileName = "file_"+ dateTimeStamp + "_" + res.originalname;
+        callback(null, fileName);
+    }
+});
+
+var upload = multer({ storage: Storage }).single("imgUploader"); 
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+var filePath;
+var filePaths = [];
+var list;
+app.post("/upload", function (req, res) {
+
+    upload(req, res, function (err) {      
+        if (err) 
+            res.send("Oops.....upload fail")
+        else {
+            var fileType = require('mime').lookup(fileName).split("/")[1]
+            if (fileType == "zip"){
+                fs.createReadStream('./upload/'+ fileName).pipe(unzip.Extract({ path: 'upload/unzip' }));
+                fs.readdir('./upload/unzip/', (err, files)=>{
+                    files.forEach(file => {
+                        filePaths.push('./upload/unzip/'+file);
+                    });
+                    
+                    res.send(filePaths);
+                })
+                        
+
+            }
+            else
+                res.redirect('./upload/'+fileName);
+        
+        }
+        
+    });
+});
+
+app.listen(port);
